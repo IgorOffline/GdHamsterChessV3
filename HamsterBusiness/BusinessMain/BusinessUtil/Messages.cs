851bd18df268e7ebed527217be5b0dc90fe74a53
Igor Durbek <igor@igordurbek.com>
2024-09-08 08:09:14 +0200

    HamsterBusiness moved one directory up (to project root)

--- HamsterBusiness/BusinessMain/BusinessUtil/Messages.cs
+++ HamsterBusiness/BusinessMain/BusinessUtil/Messages.cs
@@ -2,6 +2,7 @@
 
 public class Messages
 {
+    public const string LoremIpsum = "Lorem Ipsum";
     public const string UnknownLetter = "Unknown Letter";
     public const string UnknownNumber = "Unknown Number";
     public const string UnknownPieceColor = "Unknown Piece Color";
--- HamsterChess/scripts/Hamster.cs
+++ HamsterChess/scripts/Hamster.cs
@@ -1,11 +1,12 @@
 using Godot;
 using System;
+using HamsterBusiness.BusinessMain.BusinessUtil;
 
 public partial class Hamster : Node2D
 {
     public override void _Ready()
     {
-        GD.Print("Hamster");
+        GD.Print("Hamster; " + Messages.LoremIpsum);
     }
 
     public override void _Process(double delta)
